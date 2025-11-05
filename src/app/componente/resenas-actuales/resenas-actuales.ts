d53ce0293863a042fa33d9445165e8082c8737a9
Jhon Jimenez <u20231c873@upc.edu.pe>
2025-11-05 10:35:43 -0500

    Implementación de resenas-actuales.

---
+++ src/app/componente/resenas-actuales/resenas-actuales.ts
@@ -0,0 +1,217 @@
+import {ChangeDetectorRef, Component, HostListener, inject} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {RouterLink} from '@angular/router';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-resenas-actuales',
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+    NgOptimizedImage
+  ],
+  templateUrl: './resenas-actuales.html',
+  styleUrl: './resenas-actuales.css',
+})
+export class ResenasActuales {
+  buscarForm: FormGroup;
+  buscarAvanzadaForm: FormGroup;
+  mostrarFiltrosAvanzados = false;
+  menuPerfilActivo = false;
+  menuActivo = false;
+  animando = false;
+  mostrarCerrarSesion = false;
+  private fb: FormBuilder = inject(FormBuilder);
+
+  eventoActual = {
+    // Asegúrate de tener estas imágenes en /assets
+    imagen: '/assets/evento-boda-dia.png',
+    titulo: 'Festival de música clásica',
+    descripcion: 'Boda. Un día único y especial, lleno de amor, alegría y unión...',
+    precio: 'S/.25000.00',
+    personas: 250,
+    fecha: '25/11/2025',
+    status: 'resena no publicada aún'
+  };
+
+  // Lista de resenas
+  resenas = [
+    {
+      // Asegúrate de tener estas imágenes en /assets
+      avatar: '/assets/avatar-marie.png',
+      nombre: 'Marie Cacinetti',
+      rating: 4.0,
+      comentario: 'Una buena experiencia, la recomiendo.'
+    },
+    {
+      avatar: '/assets/avatar-roberto.png',
+      nombre: 'Roberto Miranda',
+      rating: 3.0,
+      comentario: 'Experiencia regular, esta bien supongo...'
+    },
+    {
+      avatar: '/assets/avatar-antonia.png',
+      nombre: 'Antonia Berbelotti',
+      rating: 4.0,
+      comentario: 'La mejor experiencia de la vida.'
+    }
+  ];
+
+
+  constructor(private cdr: ChangeDetectorRef) {
+    this.buscarForm = this.fb.group({
+      Donde: ['', Validators.required],
+      Aforo: ['', Validators.required],
+      Fecha: ['', Validators.required],
+    })
+    this.buscarAvanzadaForm = this.fb.group({
+      UbicacionAvanzada: ['', Validators.required],
+      TipoEventoAvanzada: ['', Validators.required],
+      FechaInicioAvanzada: ['', Validators.required],
+      FechaFinAvanzada: ['', Validators.required],
+      AforoAvanzada: ['', Validators.required],
+      PresupuestoAvanzada: ['', Validators.required],
+    })
+
+
+
+
+  }
+  cerrarMenu(menu: Element, boton: Element) {
+    menu.classList.remove('activo');
+    menu.classList.add('saliendo');
+    boton.classList.remove('activo');
+    this.menuActivo = false;
+  }
+  toggleMenu() {
+    if (this.animando) return;
+    this.animando = true;
+    const menu = document.querySelector('.menu-hamburguesa-text');
+    const boton = document.querySelector('.menu-hamburguesa-boton');
+    if (!menu || !boton) return;
+    if (!this.menuActivo) {
+      menu.classList.remove('saliendo');
+      menu.classList.add('activo');
+      boton.classList.add('activo');
+      this.menuActivo = true;
+    } else {
+      menu.classList.remove('activo');
+      menu.classList.add('saliendo');
+      boton.classList.remove('activo');
+      this.menuActivo = false;
+    }
+    setTimeout(() => (this.animando = false), 600);
+  }
+  toggleFiltrosAvanzados() {
+    if (this.animando) return;
+    this.animando = true;
+    const overlay = document.querySelector('.overlay-buscar-avanzada');
+    if (!overlay) return;
+    const menuPerfil = document.querySelector('.encabezado-perfil-menu');
+    if (this.menuPerfilActivo && menuPerfil) {
+      this.cerrarMenuPerfil(menuPerfil);
+    }
+    const menuHamb = document.querySelector('.menu-hamburguesa-text');
+    const botonHamb = document.querySelector('.menu-hamburguesa-boton');
+    if (this.menuActivo && menuHamb && botonHamb) {
+      this.cerrarMenu(menuHamb, botonHamb);
+    }
+    if (!this.mostrarFiltrosAvanzados) {
+      overlay.classList.remove('saliendo');
+      overlay.classList.add('activo');
+      this.mostrarFiltrosAvanzados = true;
+    } else {
+      overlay.classList.remove('activo');
+      overlay.classList.add('saliendo');
+      this.mostrarFiltrosAvanzados = false;
+    }
+    setTimeout(() => (this.animando = false), 600);
+  }
+  abrirFiltros() {
+    this.mostrarFiltrosAvanzados = true;
+  }
+  cerrarFiltros() {
+    this.mostrarFiltrosAvanzados = false;
+  }
+  cerrarFiltrosAvanzados() {
+    const overlay = document.querySelector('.overlay-buscar-avanzada');
+    if (!overlay) return;
+    overlay.classList.remove('activo');
+    overlay.classList.add('saliendo');
+    this.mostrarFiltrosAvanzados = false;
+  }
+  toggleMenuPerfil() {
+    if (this.animando) return;
+    this.animando = true;
+    const menuPerfil = document.querySelector('.encabezado-perfil-menu');
+    const overlay = document.querySelector('.overlay-buscar-avanzada');
+    if (this.mostrarFiltrosAvanzados && overlay) {
+      overlay.classList.remove('activo');
+      overlay.classList.add('saliendo');
+      this.mostrarFiltrosAvanzados = false;
+    }
+    const menuHamb = document.querySelector('.menu-hamburguesa-text');
+    const botonHamb = document.querySelector('.menu-hamburguesa-boton');
+    if (this.menuActivo && menuHamb && botonHamb) {
+      this.cerrarMenu(menuHamb, botonHamb);
+    }
+    if (!this.menuPerfilActivo) {
+      menuPerfil?.classList.remove('saliendo');
+      menuPerfil?.classList.add('activo');
+      this.menuPerfilActivo = true;
+    } else {
+      menuPerfil?.classList.remove('activo');
+      menuPerfil?.classList.add('saliendo');
+      this.menuPerfilActivo = false;
+    }
+    setTimeout(() => (this.animando = false), 600);
+  }
+  cerrarMenuPerfil(menuPerfil: Element) {
+    menuPerfil.classList.remove('activo');
+    menuPerfil.classList.add('saliendo');
+    this.menuPerfilActivo = false;
+  }
+  cerrarSesion() {
+    this.mostrarCerrarSesion = false;
+  }
+  confirmarCerrarSesion(event: MouseEvent) {
+    event.stopPropagation();
+    this.mostrarCerrarSesion = false;
+    document.body.classList.remove('modal-abierto');
+  }
+  cancelarCerrarSesion(event: MouseEvent) {
+    event.stopPropagation();
+    this.mostrarCerrarSesion = false;
+    document.body.classList.remove('modal-abierto');
+  }
+  abrirModalCerrarSesion() {
+    this.mostrarCerrarSesion = true;
+    document.body.classList.add('modal-abierto');
+  }
+  @HostListener('document:click', ['$event'])
+  onClickFuera(event: MouseEvent) {
+    if (this.mostrarCerrarSesion) return;
+    const menu = document.querySelector('.menu-hamburguesa-text');
+    const boton = document.querySelector('.menu-hamburguesa-boton');
+    const menuPerfil = document.querySelector('.encabezado-perfil-menu');
+    const botonPerfil = document.querySelector('.encabezado-perfil-container');
+    const target = event.target as HTMLElement;
+    if (
+      this.menuActivo && menu && boton && !menu.contains(target) && !boton.contains(target)) {
+      this.cerrarMenu(menu, boton);
+    }
+    if (this.menuPerfilActivo && menuPerfil && botonPerfil && !menuPerfil.contains(target) && !botonPerfil.contains(target)) {
+      this.cerrarMenuPerfil(menuPerfil);
+    }
+  }
+
+
+  /*RESENAS ACTUALES */
+  getStars(rating: number): any[] {
+    const fullStars = Math.floor(rating);
+    const emptyStars = 5 - fullStars;
+
+    // Devuelve [true, true, true, false, false] para un rating de 3
+    return new Array(fullStars).fill(true).concat(new Array(emptyStars).fill(false));
+  }
+}
